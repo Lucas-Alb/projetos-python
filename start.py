f7a4a7903d9709ba4b96bfb979c3457a0fef86f7
Lucas-Albuquerque-CC <lucasalbuquerque.network@gmail.com>
2025-03-27 23:45:36 -0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,29 @@
+# Calculadora simples
+
+# Mostra as opções de operações disponíveis
+print("Operações disponíveis: \nMultiplicação: * \nDivisão: / \nSoma: + \nSubtração: - \nExponenciação/potenciação: \nRaiz: ")
+
+# Variáveis e inputs para a oscolha da operação e insersão dos números
+operacao = str(input("Digite a operação desejada: "))
+
+if operacao == "^":                                     #para fazer a raiz quadrada, só precisa de um numero
+    numero_raiz = float(input("Digite um numero: "))    #então foi posto o if para pedir apenas um número
+else:
+    numero1 = float(input("Digite um numero: "))
+    numero2 = float(input("Digite um numero: "))
+
+# Código para as operações
+if operacao == "**":
+    print(f"{numero1 ** numero2:.2f}")
+elif operacao == "^":                        # poderia usar sqrt(), mas ele só funciona com numeros inteiros
+    print(f"{numero_raiz ** (1/2):.2f}")     # como é float ele retornaria um erro dependendo do numero
+elif operacao == "*":
+    print(f"{numero1 * numero2:.2f}")
+elif operacao == "/":
+    print(f"{numero1 / numero2:.2f}")
+elif operacao == "+":
+    print(f"{numero1 + numero2:.2f}")
+elif operacao == "-":
+    print(f"{numero1 - numero2:.2f}")
+else:
+    print("Operação invalida")
